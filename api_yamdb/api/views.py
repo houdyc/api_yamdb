e158456f5f34d9e110facdb7e8884c51c3a6cba1
loren166 <107641713+loren166@users.noreply.github.com>
2023-02-13 13:15:09 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -70,26 +70,17 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class CategoryViewSet(
-    ListModelMixin, CreateModelMixin, DestroyModelMixin, GenericViewSet
+    CreateModelMixin,
+    ListModelMixin,
+    DestroyModelMixin,
+    GenericViewSet
 ):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminOrReadOnlyPermission]
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-
-    @action(
-        detail=False,
-        methods=['delete'],
-        url_path=r'(?P<slug>\w+)',
-        lookup_field='slug',
-        url_name='category_slug',
-    )
-    def get_category(self, request, slug):
-        category = self.get_object()
-        serializer = CategorySerializer(category)
-        category.delete()
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    lookup_field = 'slug'
 
 
 class GenreViewSet(
